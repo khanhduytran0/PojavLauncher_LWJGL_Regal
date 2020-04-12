b91f7d5ea4e2c59cd4c841bae5b6bc8e2c213404
khanhduytran0 <40482367+khanhduytran0@users.noreply.github.com>
2020-04-12 18:06:28 +0700

    Upload missing header files
    Because unable to pull/push after merge.

---
+++ jni/common/opengl/org_lwjgl_opengl_CallbackUtil.h
@@ -0,0 +1,22 @@
+/* This file was automatically generated.  Do not edit! */
+
+#ifndef __org_lwjgl_opengl_CallbackUtil_h_
+#define __org_lwjgl_opengl_CallbackUtil_h_
+
+#include <jni.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT jlong JNICALL Java_org_lwjgl_opengl_CallbackUtil_getDebugCallbackKHR(JNIEnv *env,jclass clazz);
+JNIEXPORT jlong JNICALL Java_org_lwjgl_opengl_CallbackUtil_getDebugOutputCallbackAMD(JNIEnv *env,jclass clazz);
+JNIEXPORT jlong JNICALL Java_org_lwjgl_opengl_CallbackUtil_getDebugOutputCallbackARB(JNIEnv *env,jclass clazz);
+JNIEXPORT void JNICALL Java_org_lwjgl_opengl_CallbackUtil_deleteGlobalRef(JNIEnv *env,jclass clazz,jlong globalRef);
+JNIEXPORT jlong JNICALL Java_org_lwjgl_opengl_CallbackUtil_ncreateGlobalRef(JNIEnv *env,jclass clazz,jobject obj);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __org_lwjgl_opengl_CallbackUtil_h_
---
+++ jni/common/opengl/org_lwjgl_opengl_GLContext.h
@@ -0,0 +1,21 @@
+/* This file was automatically generated.  Do not edit! */
+
+#ifndef __org_lwjgl_opengl_GLContext_h_
+#define __org_lwjgl_opengl_GLContext_h_
+
+#include <jni.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_org_lwjgl_opengl_GLContext_resetNativeStubs(JNIEnv *env,jclass clazz,jclass gl_class);
+JNIEXPORT void JNICALL Java_org_lwjgl_opengl_GLContext_nUnloadOpenGLLibrary(JNIEnv *env,jclass clazz);
+JNIEXPORT void JNICALL Java_org_lwjgl_opengl_GLContext_nLoadOpenGLLibrary(JNIEnv *env,jclass clazz);
+JNIEXPORT jlong JNICALL Java_org_lwjgl_opengl_GLContext_ngetFunctionAddress(JNIEnv *env,jclass clazz,jlong function_name);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __org_lwjgl_opengl_GLContext_h_
